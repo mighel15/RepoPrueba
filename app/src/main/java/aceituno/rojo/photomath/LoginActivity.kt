9058ea1b18985aea629291acb5b8d2e6ba4e2261
Miguel Aceituno Rojo <miguel.aceituno@soltechsac.com>
2024-05-21 17:58:30 -0500

    mi primera subida de proyecto

---
+++ app/src/main/java/aceituno/rojo/photomath/LoginActivity.kt
@@ -0,0 +1,29 @@
+package aceituno.rojo.photomath
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnNext = findViewById<Button>(R.id.btnNext)
+        btnNext.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
