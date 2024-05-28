539e364002db2593e9f59e29f534dbf6da584485
Miguel Aceituno Rojo <miguel.aceituno@soltechsac.com>
2024-05-28 18:39:05 -0500

    Agregando la pantalla login

--- app/src/main/java/aceituno/rojo/photomath/LoginActivity.kt
+++ app/src/main/java/aceituno/rojo/photomath/LoginActivity.kt
@@ -2,23 +2,25 @@ package aceituno.rojo.photomath
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
-
+    //private lateinit var tvMensaje :TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        //tvMensaje = findViewById(R.id.tvMensaje)
+
+        //tvMensaje.setOnClickListener {
+        //    val intentNextActivity = Intent(this, WelcomeActivity  ::class.java)
+        //    startActivity(intentNextActivity)
+        //}
 
 
     }
---
+++ app/src/main/java/aceituno/rojo/photomath/WelcomeActivity.kt
@@ -0,0 +1,28 @@
+package aceituno.rojo.photomath
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var btnIniciar : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome)
+
+        btnIniciar = findViewById(R.id.btnIngresar)
+
+        btnIniciar.setOnClickListener{
+            //Toast.makeText(baseContext, "Me hicieron clic", Toast.LENGTH_SHORT).show()
+
+            val intentNextActivity = Intent(this, LoginActivity::class.java)
+            startActivity(intentNextActivity)
+        }
+    }
+}
