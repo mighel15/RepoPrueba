365f7fe997312d3bd345a7045d74ff1ab639aeb2
Miguel Aceituno Rojo <miguel.aceituno@soltechsac.com>
2024-06-03 16:10:18 -0500

    La identificación y el menu principal

--- app/src/main/java/aceituno/rojo/photomath/LoginActivity.kt
+++ app/src/main/java/aceituno/rojo/photomath/LoginActivity.kt
@@ -2,7 +2,10 @@ package aceituno.rojo.photomath
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,24 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
+
+        val btnIngresar = findViewById<Button>(R.id.btnIngresar)
+        val etUsuario = findViewById<EditText>(R.id.etUsuario)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+
+        btnIngresar.setOnClickListener {
+            if(etUsuario.text.toString().trim() == "admin" && etPassword.text.toString().trim() == "123456")
+            {
+                //ingreso al menu
+                val intentNextActivity = Intent(this, MenuActivity::class.java)
+                startActivity(intentNextActivity)
+            }
+            else{
+                Toast.makeText(this, "Usuario o contraseña incorrecta", Toast.LENGTH_LONG).show()
+            }
+
+
+        }
         //tvMensaje = findViewById(R.id.tvMensaje)
 
         //tvMensaje.setOnClickListener {
